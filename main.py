2c6c46b4b8da144fb3303ad8914e39473fcba075
Rama Hany <70003522+ramahany@users.noreply.github.com>
2025-04-25 21:05:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,55 +7,7 @@ import cv2
 import numpy as np
 
 
-def add_to_report(score, pos, image):
-
-    now = datetime.now()
-    try:
-        firebase_admin.get_app()
-    except ValueError:
-        firebase_admin.initialize_app()
-    db = firestore.client()
-    doc_ref = db.collection("users").document(st.session_state.user_id)
-    report_ref = doc_ref.collection("scores").document(pos)
-
-    # s = f"{str(pos).replace(' ', '_')}_states"
-    old_data = doc_ref.get().to_dict()[f"{str(pos).replace(' ', '_')}_states"]
-    new_data = {
-        "avg": (old_data["avg"] * old_data["count"] + score) / (old_data["count"] + 1) if old_data["avg"] > 0 else score,
-        "max": max(old_data["max"], score ),
-        "min": min(old_data["min"], score ),
-        "last": score,
-        "count": old_data["count"] + 1
-    }
-  
-
-    # Adding  the IMAGES to storage
-    try:
-        # TODO save the bucket so its only created once 
-        bucket = storage.bucket("forms-data-e0050.appspot.com")
-        blob = bucket.blob(f'UsersData/EvaluatedImages/{st.session_state.user_id}{pos}{new_data["count"]}.png')
-        blob.upload_from_string(image, content_type='image/png')
-        blob.make_public()
-        # url = blob.public_url
-        report_ref.set({
-        str(now):
-        {
-        "Scroe": score,
-        "image": blob.public_url
-        }
-        }, merge=True)
-        doc_ref.update({f"{pos.replace(' ', '_')}_states": new_data})
-    except Exception as e:
-        st.error(f"Error occurred while submiting your score, please try again!")
-    else:
-        st.success(f"score added to {pos} report")
-
-
-
-
-data = st.session_state.user
-name = data['name']
-st.header(f'welcome back {name}')
+st.title(f'welcome back r4m4 ')
 poses = ["front balance", "side balance"]
 pos = st.selectbox("evaluation for ...", poses, index=None, placeholder="Select pose ...")
 image = st.file_uploader("Choose an image...", type=["jpg", "png"])
@@ -71,7 +23,6 @@ if image is not None:
             for line in feedback:
                 st.write(line)
 
-            st.page_link("user/reference.py", label="Go to References")
 
         is_success, buffer = cv2.imencode(".png", out_img)
         io_buf = buffer.tobytes()
@@ -83,7 +34,6 @@ if image is not None:
             file_name="image.png",
             mime="image/png"
         )
-        st.button("submit", on_click=add_to_report, args=(score, pos, io_buf))
     else:
         st.error('invalid image, please upload another image!', icon="🚨")
 
